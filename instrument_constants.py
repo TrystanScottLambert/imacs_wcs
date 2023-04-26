7008046a4a9cf9bab2176c18331a361a8a9b1389
Trystan Lambert <trystanscottlambert@gmail.com>
2023-04-26 13:58:16 -0400

    refactoring

--- instrument_constants.py
+++ instrument_constants.py
@@ -7,7 +7,7 @@ class Constants:
     """All of the constants that are needed."""
 
     iroa_d: float = -46.25
-    offx: float = 4240
-    offy: float = 4240
-    nx_num: float = 8800
-    ny_num: float = 8800
+    offx: int = 4240
+    offy: int = 4240
+    nx_num: int = 8800
+    ny_num: int = 8800
--- manual_astrometry.py
+++ manual_astrometry.py
@@ -18,6 +18,8 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from astroquery.gaia import Gaia
 
+from draggable_scatter import DraggableScatter
+
 
 def are_points_aligned(data: np.ndarray, x_array: np.array, y_array: np.ndarray) -> bool:
     """
@@ -39,9 +41,8 @@ def are_points_aligned(data: np.ndarray, x_array: np.array, y_array: np.ndarray)
     return counter/len(x_array) < 0.7
 
 
-def search_gaia_archives(
-        ra: float, dec: float, height:float, width:float)\
-              -> Tuple[np.ndarray, np.ndarray]:
+def search_gaia_archives(ra: float, dec: float, height:float, width:float)\
+    -> Tuple[np.ndarray, np.ndarray]:
     """
     Does a box search centered on ra and dec with a given height and width.
     """
@@ -57,7 +58,8 @@ def sum_columns_and_rows(array_2d: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     row_sum = np.sum(array_2d,axis=1)
     return column_sum, row_sum
 
-def gauss(x_array: np.ndarray, constant:float, amplitude:float, mean:float, sigma:float):
+def gauss(x_array: np.ndarray, constant:float, amplitude:float, mean:float, sigma:float)\
+      -> np.ndarray:
     """Definiton of a gaussian."""
     return constant + amplitude * np.exp(-(x_array - mean) ** 2 / (2 * sigma ** 2))
 
@@ -68,7 +70,7 @@ def gauss_fit(x_array: np.ndarray, y_array:np.ndarray) -> np.ndarray:
     popt, _ = curve_fit(gauss, x_array, y_array, p0=[np.min(y_array), np.max(y_array), mean, sigma])
     return popt
 
-def get_star_position(star: np.ndarray):
+def get_star_position(star: np.ndarray) -> tuple[float, float]:
     """Determines the two 1-d gaussian fit positoin of the cut out star."""
     column_sum, row_sum = sum_columns_and_rows(star)
     x_values = np.arange(len(row_sum))
@@ -77,7 +79,8 @@ def get_star_position(star: np.ndarray):
 
     return column_popt[2], row_popt[2]
 
-def cut_star(x_position,y_position,image_data):
+def cut_star(x_position: float, y_position: float, image_data: np.ndarray)\
+      -> tuple[np.ndarray, float, float]:
     """Cuts out star and determines the center of said star."""
     x_position_rounded, y_position_rounded = int(round(x_position)), int(round(y_position))
     padding = 20
@@ -95,75 +98,6 @@ def cut_star(x_position,y_position,image_data):
         return image_postage_stamp, accurate_x_position, accurate_y_position
     except (RuntimeError, ValueError, TypeError):
         return None
-
-
-class DraggableScatter():
-    """Class for a scatter plot that the user can drag with the mouse."""
-    epsilon = 5
-
-    def __init__(self, scatter):
-        "Initiliazing."
-        self.scatter = scatter
-        self._ind = None
-        self.ax = scatter.axes
-        self.canvas = self.ax.figure.canvas
-        self.canvas.mpl_connect('button_press_event', self.button_press_callback)
-        self.canvas.mpl_connect('button_release_event', self.button_release_callback)
-        self.canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
-        plt.show()
-
-
-    def get_ind_under_point(self, event):
-        "Idenifying the scatter point we are clickin."
-        xy = np.asarray(self.scatter.get_offsets())
-        xyt = self.ax.transData.transform(xy)
-        xt, yt = xyt[:, 0], xyt[:, 1]
-
-        d = np.sqrt((xt - event.x)**2 + (yt - event.y)**2)
-        ind = d.argmin()
-
-        if d[ind] >= self.epsilon:
-            ind = None
-        return ind, xy[ind]
-
-    def button_press_callback(self, event):
-        """Clicking the button"""
-        if event.inaxes is None:
-            return
-        if event.button != 1:
-            return
-        self._ind, pos = self.get_ind_under_point(event)
-        self.x_ref = pos[0]
-        self.y_ref = pos[1]
-
-
-    def button_release_callback(self, event):
-        """Letting go of the button"""
-        if event.button != 1:
-            return
-        self._ind = None
-
-        xy = np.asarray(self.scatter.get_offsets())
-        x_offset = event.xdata - self.x_ref
-        y_offset = event.ydata - self.y_ref
-
-        #xy[self._ind] = np.array([x, y])
-        for idx, _ in enumerate(xy):
-            xy[idx] = np.array([xy[idx][0]+x_offset, xy[idx][1]+y_offset])
-
-        self.scatter.set_offsets(xy)
-        self.canvas.draw_idle()
-        self.positions = xy
-
-
-    def motion_notify_callback(self, event):
-        """Moving the mouse"""
-        if self._ind is None:
-            return
-        if event.inaxes is None:
-            return
-        if event.button != 1:
-            return
 
 
 def get_usable_gaia(gaia_x_array: np.ndarray, gaia_y_array: np.ndarray, image: np.ndarray):
@@ -203,7 +137,7 @@ class ChipImage:
         self.current_gaia_y = current_gaia_info[3]
         interval = ZScaleInterval()
         self.vmin, self.vmax = interval.get_limits(self.data)
-    
+
     def query_gaia(self):
         """Gets the gaia positions in the chip frame."""
         center_y_pix = self.data.shape[0]/2
@@ -214,23 +148,25 @@ class ChipImage:
         search_height = self.data.shape[0] * np.abs(self.header['PC1_1'])
 
         center_ra, center_dec = self.current_wcs.pixel_to_world_values(center_x_pix, center_y_pix)
-        gaia_ra, gaia_dec = search_gaia_archives(center_ra, center_dec, width = search_width, height=search_height)
+        gaia_ra, gaia_dec = search_gaia_archives(
+            center_ra, center_dec, width = search_width, height=search_height)
         gaia_x, gaia_y = self.current_wcs.world_to_pixel_values(gaia_ra, gaia_dec)
 
         return gaia_ra, gaia_dec, gaia_x, gaia_y
-    
+
     def _manually_determine_gaia_offsets(self):
         """
-        Starts the draggable plot for alignment. And uses user input to determine the pixel offsets of 
-        the GAIA stars.
+        Starts the draggable plot for alignment and uses user input to determine
+        the pixel offsets of the GAIA stars.
         """
 
         fig = plt.figure()
-        ax = fig.add_subplot(projection=self.current_wcs)
-        ax.set_title(self.file_name)
-        ax.imshow(self.data, vmin=self.vmin, vmax=self.vmax, cmap='gray')
-        scatter = ax.scatter(
-            self.current_gaia_x, self.current_gaia_y, facecolor='None', edgecolor='r', marker='s', s=80, picker=True)
+        ax_align = fig.add_subplot(projection=self.current_wcs)
+        ax_align.set_title(self.file_name)
+        ax_align.imshow(self.data, vmin=self.vmin, vmax=self.vmax, cmap='gray')
+        scatter = ax_align.scatter(
+            self.current_gaia_x, self.current_gaia_y, facecolor='None',
+            edgecolor='r', marker='s', s=80, picker=True)
         DraggableScatter(scatter)
         plt.show()
         updated_gaia_x, updated_gaia_y = scatter.get_offsets().T
@@ -238,42 +174,57 @@ class ChipImage:
         offset_y = updated_gaia_y - self.current_gaia_y
         return offset_x, offset_y
 
-    def determine_wcs_manually(self, show_alignment: bool = False) -> WCS:
+    def _overplot_scatter(self, x_scatter: np.ndarray, y_scatter: np.ndarray):
         """
-        Using draggabale scatter plot to determine the x,y offsets.
+        Plots scatter points over the chip data.
         """
-        gaia_offset_x, gaia_offset_y = self._manually_determine_gaia_offsets() # starts interactive plot
+        fig = plt.figure()
+        ax_scatter = fig.add_subplot(projection=self.current_wcs)
+        ax_scatter.set_title(self.file_name)
+        ax_scatter.imshow(self.data, vmin=self.vmin, vmax=self.vmax, cmap='gray')
+        ax_scatter.scatter(
+            x_scatter, y_scatter, facecolor='None', edgecolor='r', marker='s', s=100, picker=True)
+        plt.show()
+
+    def _fit_gaia_coords(self, gaia_offset_x: np.ndarray, gaia_offset_y: np.ndarray) \
+          -> tuple[SkyCoord, np.ndarray]:
+        """
+        Uses offset to align gaia points then fits the these points accurately
+        returning their onsky position and their matching xy values.
+        """
         updated_gaia_x = self.current_gaia_x + gaia_offset_x
         updated_gaia_y = self.current_gaia_y + gaia_offset_y
-        accurate_gaia_x, accurate_gaia_y, msk = get_usable_gaia(updated_gaia_x, updated_gaia_y, self.data)
+        accurate_gaia_x, accurate_gaia_y, msk = get_usable_gaia(
+            updated_gaia_x, updated_gaia_y, self.data)
 
-        diff = np.hypot(updated_gaia_x[msk] - accurate_gaia_x, updated_gaia_y[msk] - accurate_gaia_y)
+        diff = np.hypot(updated_gaia_x[msk]-accurate_gaia_x, updated_gaia_y[msk]-accurate_gaia_y)
         _, diff_median, diff_std = sigma_clipped_stats(diff)
         cut = np.where(diff < diff_median + 1*diff_std)[0]
 
         self.gaia_offset_x = np.mean(accurate_gaia_x[cut] - self.current_gaia_x[msk][cut])
         self.gaia_offset_y = np.mean(accurate_gaia_y[cut] - self.current_gaia_y[msk][cut])
 
-
-
-        gaia_coords = [(self.gaia_ra[msk][cut][i], self.gaia_dec[msk][cut][i]) for i, _ in enumerate(self.gaia_ra[msk][cut])]
+        gaia_coords = [(self.gaia_ra[msk][cut][i], self.gaia_dec[msk][cut][i]) \
+                        for i, _ in enumerate(self.gaia_ra[msk][cut])]
         gaia_skycoords = SkyCoord(gaia_coords, unit=(u.deg, u.deg))
         gaia_pixcoords = np.array([accurate_gaia_x[cut], accurate_gaia_y[cut]])
+        return gaia_skycoords, gaia_pixcoords
+
+
+    def determine_wcs_manually(self, show_alignment: bool = False) -> WCS:
+        """
+        Using draggabale scatter plot to determine the x,y offsets.
+        """
+        gaia_offset_x, gaia_offset_y = self._manually_determine_gaia_offsets()
+        gaia_skycoords, gaia_pixcoords = self._fit_gaia_coords(gaia_offset_x, gaia_offset_y)
+
+        if show_alignment:
+            self._overplot_scatter(gaia_pixcoords[0], gaia_pixcoords[1])
 
         wcs = utils.fit_wcs_from_points(gaia_pixcoords, gaia_skycoords)
 
-        if show_alignment:
-
-            fig = plt.figure()
-            ax = fig.add_subplot(projection=self.current_wcs)
-            ax.set_title(self.file_name)
-            ax.imshow(self.data, vmin=self.vmin, vmax=self.vmax, cmap='gray')
-            ax.scatter(accurate_gaia_x[cut], accurate_gaia_y[cut], facecolor='None', edgecolor='r', marker='s', s=100, picker=True)
-            plt.show()
-
         return wcs
 
-    
     def determine_wcs_with_offsets(
             self, x_pix_offset: np.ndarray, y_pix_offset: np.ndarray,
             show_alignment: bool = False) -> WCS:
@@ -281,36 +232,16 @@ class ChipImage:
         Using a set of offsets to try fit the data. 
         If these offset fail then manual wcs will be needed.
         """
-        gaia_offset_x, gaia_offset_y = x_pix_offset, y_pix_offset
-        updated_gaia_x = self.current_gaia_x + gaia_offset_x
-        updated_gaia_y = self.current_gaia_y + gaia_offset_y
-        accurate_gaia_x, accurate_gaia_y, msk = get_usable_gaia(updated_gaia_x, updated_gaia_y, self.data)
+        gaia_skycoords, gaia_pixcoords = self._fit_gaia_coords(x_pix_offset, y_pix_offset)
 
-        diff = np.hypot(updated_gaia_x[msk] - accurate_gaia_x, updated_gaia_y[msk] - accurate_gaia_y)
-        _, diff_median, diff_std = sigma_clipped_stats(diff)
-        cut = np.where(diff < diff_median + 1*diff_std)[0]
-
-        self.gaia_offset_x = np.mean(accurate_gaia_x[cut] - self.current_gaia_x[msk][cut])
-        self.gaia_offset_y = np.mean(accurate_gaia_y[cut] - self.current_gaia_y[msk][cut])
-
-        gaia_coords = [(self.gaia_ra[msk][cut][i], self.gaia_dec[msk][cut][i]) for i, _ in enumerate(self.gaia_ra[msk][cut])]
-        gaia_skycoords = SkyCoord(gaia_coords, unit=(u.deg, u.deg))
-        gaia_pixcoords = np.array([accurate_gaia_x[cut], accurate_gaia_y[cut]])
+        if show_alignment:
+            self._overplot_scatter(gaia_pixcoords[0], gaia_pixcoords[1])
 
         if are_points_aligned(self.data, gaia_pixcoords[0], gaia_pixcoords[1]):
             wcs = utils.fit_wcs_from_points(gaia_pixcoords, gaia_skycoords)
         else:
             print('Offsets failed to fit. Manual intervention required.')
             wcs = self.determine_wcs_manually()
-            
-        if show_alignment:
-
-            fig = plt.figure()
-            ax = fig.add_subplot(projection=self.current_wcs)
-            ax.set_title(self.file_name)
-            ax.imshow(self.data, vmin=self.vmin, vmax=self.vmax, cmap='gray')
-            ax.scatter(accurate_gaia_x[cut], accurate_gaia_y[cut], facecolor='None', edgecolor='r', marker='s', s=100, picker=True)
-            plt.show()
 
         return wcs
 
